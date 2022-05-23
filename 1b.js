9a0ad659787aa7d7f301e4d2824b15642c3989ca
im ranjitha <ranjithatk101@gmail.com>
2022-05-23 14:06:13 +0530

    changes

--- 1b.js
+++ 1b.js
@@ -17,11 +17,19 @@ const student = {
   },
 }
 // console.log(student.skills.frontEnd[0])
-function newStudent(skill, level) {
-  student.skills.frontEnd.push({ skill, level })
+function newStudent(array, value) {
+  array.push(value)
   return student.skills
 }
-console.log(newStudent("bootstrap", 8))
+console.log(
+  newStudent(student.skills.backEnd, { skills: "bootstrap", level: 8 })
+)
+// function newStudent(skill, level) {
+//   student.skills.frontEnd.push({ skill, level })
+//   return student.skills
+// }
+// console.log(newStudent("bootstrap", 8))
+
 // student.skills.frontEnd.push(
 //   (student.frontEnd[skills] = "bootstrap"),
 //   (student.frontEnd[level] = 8)
